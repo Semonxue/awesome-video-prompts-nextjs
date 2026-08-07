3ce2fcaa38942452289b5c777674c7453f117643
Semon Xue <semonxue@gmail.com>
2026-08-08 01:52:34 +0800

    update version

--- src/version-generated.ts
+++ src/version-generated.ts
@@ -1,5 +1,5 @@
 // 此文件由 prebuild 自动生成，不要手动修改
 export const BUNDLED_VERSION = '2.0.1' as const;
-export const BUNDLED_SHA = 'e9a00d8' as const;
-export const BUNDLED_DATE = '2026-08-07 16:28:43' as const;
+export const BUNDLED_SHA = '588aaed' as const;
+export const BUNDLED_DATE = '2026-08-08 00:43:46' as const;
 export const BUNDLED_BRANCH = 'main' as const;
